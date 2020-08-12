b1159e4e74c1f6901f884d6fdcf92ff3a0d54849
Daniel A <dannyboy3385@gmail.com>
2020-08-12 15:16:31 -0700

    Working README generator

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# READMECreator
+# INPUT TITLE
+> INPUT short description 
+
+LICENSE BADGE
+
+### Table of Contents:
+ INPUT table of contents
+
+
+
+## Installation:
+ INPUT installation
+
+
+## Usage:
+ INPUT usage
+
+## Contributing:
+ INPUT contributing
+
+## Tests:
+ INPUT tests
+
+### License:
+**License Used**: INPUT license 
+
+### Questions:
+**What is your GitHub username?** INPUT username
+**Email to reach for additional questions:** INPUT email
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer')
+const fs = require('fs')
 
   console.log('Create your README by answering the following prompts.')
   inquirer
@@ -36,7 +37,7 @@ const inquirer = require('inquirer')
       {
         type: 'input',
         name: 'contributing',
-        message: 'Contributing Partners:'
+        message: 'Contribution Guidelines:'
       },
       {
         type: 'input',
@@ -44,23 +45,48 @@ const inquirer = require('inquirer')
         message: 'Test Instructions:'
       },
       {
-        type: 'input',
-        name: 'questions',
-        message: 'Questions:'
-      },
-      {
-        type: 'input',
-        name: 'toc',
-        message: 'Table of Contents:'
-      },
-      {
         type: 'list',
-        name: 'licence',
+        name: 'license',
         message: 'License Type:',
         choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
       },
     ])
     .then(res => {
-      console.log(res)
+      fs.writeFile('customREADME.md', `
+# ${res.title}
+> ${res.desc}
+
+![License Badge](https://img.shields.io/badge/license-${res.license}-blue.svg)
+
+***
+### Table of Contents:
+* [Installation](#installation)
+* [Usage](#usage)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [License](#license)
+* [Questions](#questions)
+
+***
+## Installation:
+ - ${res.install}
+
+***
+## Usage:
+ - ${res.usage}
+***
+## Contributing:
+ - ${res.contributing}
+***
+## Tests:
+ - ${res.test}
+***
+### License:
+**License Used**: ${res.license}
+***
+### Questions: 
+**What is your GitHub username?** https://github.com/${res.username}
+**Email to reach for additional questions:** ${res.email}
+`, (err) => {console.log(err)})
     })
     .catch(err => {console.log(err)})
